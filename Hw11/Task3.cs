8f46a3ea22cce4f8429209de86b710c5a159bb1b
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-12 18:35:13 +0200

    Task3

---
+++ Hw11/Task3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Hw11
+{
+    class Play : IDisposable
+    {
+        public string Name { get; set; }
+
+        public Play(string name)
+        {
+            Name = name;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose для Play.");
+        }
+
+        ~Play()
+        {
+            Console.WriteLine("Деструктор Play.");
+        }
+    }
+    class Shop : IDisposable
+    {
+        public string Name { get; set; }
+
+        public Shop(string name)
+        {
+            Name = name;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose для Shop.");
+        }
+
+        ~Shop()
+        {
+            Console.WriteLine("Деструктор Shop.");
+        }
+    }
+    internal class Task3
+    {
+        static void Main(string[] args)
+        {
+            Play play = new Play("Гамлет");
+            Shop shop = new Shop("АТБ");
+
+            play.Dispose();
+            shop.Dispose();
+
+            play = null;
+            shop = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.ReadKey();
+        }
+    }
+}
